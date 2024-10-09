122380c7ccb0e1a06bdd2499e833f06dff626933
Igor Boechat <igorcboechatl@gmail.com>
2024-10-09 17:58:03 +0100

    new Car Crash

---
+++ myFirstSimpleGraphicsProject/lib/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.codeforall.fanstatics","l":"Fan"},{"p":"io.codeforall.fanstatics","l":"Main"}];updateSearchResults();
