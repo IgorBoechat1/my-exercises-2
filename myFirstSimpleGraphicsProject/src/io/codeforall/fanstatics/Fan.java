4b198f42871b656ea23a623faf813741754a8d9c
Igor Boechat <igorboechat@MacBook-Pro-de-Igor.local>
2024-10-09 10:37:33 +0100

    Simple simple graphs

--- myFirstSimpleGraphicsProject/src/io/codeforall/fanstatics/Fan.java
+++ myFirstSimpleGraphicsProject/src/io/codeforall/fanstatics/Fan.java
@@ -9,12 +9,15 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 public class Fan implements KeyboardHandler {
 
     private Keyboard keyboard;
-    private Picture picture;
+    private Picture earth;
+    private Picture background;
 
     public Fan(){
-        picture = new Picture(20, 20, "rsc/fan.jpg");
-        picture.draw();
 
+        background = new Picture(200, 200, "rsc/pixel-art-stars-moon-clouds-wallpaper-preview.jpg");
+        background.draw();
+        earth = new Picture(100, 100, "rsc/icon.png");
+        earth.draw();
         initKeyboard();
     }
     private void initKeyboard(){
@@ -24,15 +27,39 @@ public class Fan implements KeyboardHandler {
         moveRight.setKey(KeyboardEvent.KEY_RIGHT);
         moveRight.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
 
+        KeyboardEvent moveLeft = new KeyboardEvent();
+        moveLeft.setKey(KeyboardEvent.KEY_LEFT);
+        moveLeft.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+
+        KeyboardEvent moveUp = new KeyboardEvent();
+        moveUp.setKey(KeyboardEvent.KEY_UP);
+        moveUp.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+
+        KeyboardEvent moveDown = new KeyboardEvent();
+        moveDown.setKey(KeyboardEvent.KEY_DOWN);
+        moveDown.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+
         keyboard.addEventListener(moveRight);
+        keyboard.addEventListener(moveLeft);
+        keyboard.addEventListener(moveUp);
+        keyboard.addEventListener(moveDown);
     }
 
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
         switch (keyboardEvent.getKey()){
             case KeyboardEvent.KEY_RIGHT:
-                picture.translate(100, 0);
+                earth.translate(100, 0);
                 break;
+                case KeyboardEvent.KEY_LEFT:
+                    earth.translate(-100, 0);
+                    break;
+                    case KeyboardEvent.KEY_UP:
+                        earth.translate(0, -100);
+                        break;
+                        case KeyboardEvent.KEY_DOWN:
+                            earth.translate(0, 100);
+                            break;
         }
     }
 
--- myFirstSimpleGraphicsProject/src/io/codeforall/fanstatics/Main.java
+++ myFirstSimpleGraphicsProject/src/io/codeforall/fanstatics/Main.java
@@ -7,8 +7,11 @@ import org.academiadecodigo.simplegraphics.pictures.Picture;
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(10, 10, 1000, 1000);
-        rectangle.setColor(Color.CYAN);
         rectangle.fill();
+
+
+
+
 
         Fan fan = new Fan();
     }
