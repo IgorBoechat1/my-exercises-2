cc3dcc62a4042637b7ae7d40f65949023b1b1e89
Igor Boechat <igorcboechatl@gmail.com>
2024-10-25 14:08:56 +0100

    Simple web server, some variable changes

--- simple-web-server/src/io/codeforall/fanstatics/Server.java
+++ simple-web-server/src/io/codeforall/fanstatics/Server.java
@@ -5,52 +5,69 @@ import java.net.*;
 
 public class Server {
 
-    public FileInputStream fileInputStream;
-    public FileOutputStream fileOutputStream;
+    public static FileInputStream inputStream;
+    public static FileOutputStream outputStream;
+    public final static byte[] bytes = new byte[5000];
+    public static int bytesRead;
 
+public boolean fileNotfound(){
+    if (inputStream != null);
+    return false;
 
+}
     public static void main(String[] args) throws IOException {
 
         int port = 8080;
-
         BufferedInputStream reader = null;
-
         ServerSocket serverSocket = new ServerSocket(port);
 
 
-        // io.codeforall.fanstatics.Server connection
+
+        // Prepare to read and write requests from/to the client
         Socket clientSocket = serverSocket.accept();
         System.out.println("connection estabilished");
 
-        // Prepare to read and write requests from/to the client
-
-
         // Create IO's files
-
         while (true) {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String messageFromClient = in.readLine().substring(5);
             OutputStream outputStream = clientSocket.getOutputStream();
-
+            
+            
+         
+            
+            
             switch (messageFromClient) {
                 case "logo.png HTTP/1.1":
-                    byte[] bytes = new byte[5000];
-                    FileInputStream inputStream = new FileInputStream("www/favicon.ico");
-                    int bytesread = inputStream.read(bytes);
-                    System.out.println(bytesread);
-                    out.println("HTTP/1.1 200 Document Follows\r\n Content-Type: image/png \r\n Content-Length:" + bytesread + "\r\n \r\n");
-                    outputStream.write(bytes, 0, bytesread);
+
+                    inputStream = new FileInputStream("www/favicon.ico");
+
+                    bytesRead = inputStream.read(bytes);
+                    
+                    System.out.println(bytesRead);
+                    out.println("HTTP/1.1 200 Document Follows\r\n Content-Type: image/png \r\n Content-Length:" + bytesRead + "\r\n \r\n");
+
+                    outputStream.write(bytes, 0, bytesRead);
+
                     outputStream.flush();
                     break;
 
-                case " HTTP/1.1":
-                    out.println("HTTP/1.0 200 Document Follows\r\n Content-Type: text/html; charset=UTF-8\r\n Content-Length: <file_byte_size> \r\n \r\n");
-                    break;
 
                 case "404.html HTTP/1.1":
-                    out.println("HTTP/1.0 404 Not Found Content-Type: text/html; charset=UTF-8\r\n Content-Length: <file_byte_size> \r\n \r\n");
+                    FileInputStream inputStream = new FileInputStream("404.html");
+
+                    bytesRead = inputStream.read(bytes);
+
+                    System.out.println(bytesRead);
+                    out.println("HTTP/1.0 404 Not Found\r\n Content-Type: text/html; charset=UTF-8 \r\n Content-Length: <file_byte_size> \r\n \r\n");
+
+                    outputStream.write(bytes, 0, bytesRead);
+
+                    outputStream.flush();
+                    out.println("HTTP/1.0 404 Not Found\\r\\n Content-Type: text/html; charset=UTF-8 \\r\\n Content-Length: <file_byte_size> \\r\\n \\r\\n\"");
                     break;
+
                 case "index.html HTTP/1.1":
                     out.println("HTTP/1.0 200 Document Follows \r\n Content-Type: text/html; charset=UTF-8 \r\n Content-Length: <file_byte_size> A root index page \r\n \r\n");
                     break;
